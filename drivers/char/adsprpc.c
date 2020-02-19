2ad87984441c6963db79b1d66d088fe0cd8a74c7
Yaroslav Furman <yaro330@gmail.com>
2020-02-19 23:33:27 +0300

    adsprpc: force QoS to silver cores
    Per CAF but with less garbage.
    https://github.com/MiCode/Xiaomi_Kernel_OpenSource/commit/b2f356cce0adecbfecd2b37a1eca1d5d776b8ef6
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Kailash <kailash.sudhakar@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -50,6 +50,7 @@
 #include <soc/qcom/ramdump.h>
 #include <linux/debugfs.h>
 #include <linux/pm_qos.h>
+#include <linux/cpumask.h>
 #include <linux/stat.h>
 
 #define TZ_PIL_PROTECT_MEM_SUBSYS_ID 0x0C
@@ -3883,6 +3884,8 @@ static int fastrpc_internal_control(struct fastrpc_file *fl,
 		if (err)
 			goto bail;
 		mutex_lock(&fl->pm_qos_mutex);
+		fl->pm_qos_req.type = PM_QOS_REQ_AFFINE_CORES;
+		cpumask_copy(&fl->pm_qos_req.cpus_affine, cpu_lp_mask);
 		if (!fl->qos_request) {
 			pm_qos_add_request(&fl->pm_qos_req,
 				PM_QOS_CPU_DMA_LATENCY, latency);
